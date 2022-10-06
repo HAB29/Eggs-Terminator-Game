e21d6f38e985d1130b708e534c8f110af1f4350c
“HAB29” <Hab-8@live.com>
2022-10-06 07:09:35 +0300

    update 3

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Eggs-Terminator-Game-
+# The Game name 
+Eggs-Terminator
+
+# Description 
+is a Hangman game! But in a different way 😎
+ is a word-guessing game. The player has to gusses the hidden word and tries to win the game "SAVE THE DANCING EGGS FROM CRYING !"
+
+1. Pick a random word.
+2. Take the player’s guess.
+3. Quit the game if the player wants to.
+4. Check that the player’s guess is a valid letter.
+5. Keep track of letters the player has guessed.
+6. Show the player their progress.
+7. Finish when the player has guessed the word.
+
+For example, if the word were HAPPY, the first player
+would write:
+_ _ _ _ _
+
+The guessing player tries to guess the letters in the word.
+Each time they guess a letter correctly, the player fills in the
+blanks for each occurrence of that letter. For example, if the guessing player guessed the letter P, the  player would fill in the Ps
+in the word HAPPY like so:
+_ _ P P _
+
+When the guessing player guesses a letter that isn’t in the
+word, they lose a point and MAKE THE EGGS CRYING .
+
+Each time they guess a letter correctly, the first player fills in the
+blanks for each occurrence of that letter. 
+For example, if the guessing player guessed the letter E, the first player would fill in the Es
+in the word HAPPY like so:
+H _ P P _
+
+If the player Finish his All point and Made all the Eggs Sad ! before the guessing
+player guesses the word, the
+guessing player loses. ☹️
+
+# A link to the deployed live site
+
+# the approach taken
+Css , Dom Manipulation ,If Condition , Javascript
+
+# unsolved problems or growth areas you may have had
+I havent still compelted my game and if i had the time i would finish this two things 
+1. i would finshed the wining and losing condtion 
+2. i would finshed the play agian button 
+
+( i have faced some difficultes about the clickable letters and to manage the letters but i tried my best and sloved it with some guidance of my great instructors )
+
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+ //( 1)
+ 
+
+//we get the html elements
 
  //  to active the click letters
 // select all letter buttons
 let letterBtns = document.querySelectorAll('.letter');
-// console.log(letterBtns)
 //Add an eventlistener for each letter
 letterBtns.forEach(letter=>letter.addEventListener("click", letterClicked))
-
 let guesses = document.querySelector("#guesses")
-let words = ['KAREEM','ALEX','TAYLOR', "AHLAM" , "TAHER",'NAHEED']
+// Create an array of words
+let words = ['KAREEM','ALEX','AHLAM', 'TAYLOR' , 'NAHEED','TAHER']
 let lineContainer = document.querySelector('.line-container')
 let eggNum = 1
 let randomWord = words[Math.floor(Math.random() * words.length)]
@@ -26,7 +29,11 @@ randomWord.toUpperCase().split('').forEach(letter => {
 //write if else condition 
 // if words.includes(userLetter) then display letter on blank spot, else show wrong letter or display negative points
 
-//function letter clicked on the line 
+
+
+    //( 2)
+
+//i creat a function letter clicked on the line and i used  if condition for it
 
 function letterClicked (e){
     console.log(e.target.innerText)
@@ -34,13 +41,17 @@ function letterClicked (e){
     
     if (randomWord.toUpperCase().includes(userLetter)){
 
-        //correct
+        //CONSOLE LOG CORRECT! IF THEY GUSSED THE RIGHT LETTER
+
         console.log('correct!')
-        let indexOfLetter = randomWord.toUpperCase().indexOf(userLetter)
+
         // in the word Sarah, h has an index of 4
+        let indexOfLetter = randomWord.toUpperCase().indexOf(userLetter)
         console.log(lineContainer)
         console.log(indexOfLetter)
         console.log(randomWord)
+
+        //(3)
 
         lineContainer.children[indexOfLetter].innerHTML = userLetter
         // we take the ___ at index 4, and replace the _ with the letter h
@@ -51,7 +62,8 @@ function letterClicked (e){
             }
           })
     } else {
-        //incorrect
+
+       //CONSOLE LOG INCORRECT! IF THEY GUSSED THE RIGHT LETTER
         console.log('incorrect!')
         let egg = document.querySelector(`.egg${eggNum}`)
         egg.style.backgroundImage = "url('https://images.squarespace-cdn.com/content/v1/57cc635d46c3c4013750884a/1562708963560-QLIIHQDXOYT995HEVCPZ/cry.gif?format=750w')"
@@ -68,3 +80,9 @@ function letterClicked (e){
     }
   
 }
+
+// playAgain.addEventListener("click", restartGame)
+//  function restartGame () {
+//     let playAgain = document.querySelector(".playAgain")
+
+
